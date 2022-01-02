6d53ef8838918a0b1daefd66d6422b0a646c8369
ShehanUdantha <shehansub@gmail.com>
2022-01-02 14:20:55 +0530

    fourth commit

--- lib/screens/lecturer/inComponenet/Attendence/viewAttendece.dart
+++ lib/screens/lecturer/inComponenet/Attendence/viewAttendece.dart
@@ -20,6 +20,8 @@ class SubScreen extends StatefulWidget {
 class _SubScreenState extends State<SubScreen> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     CollectionReference rrId = FirebaseFirestore.instance
         .collection(widget.module)
         .doc(widget.mId)
@@ -35,41 +37,93 @@ class _SubScreenState extends State<SubScreen> {
           ],
         ),
       ),
-      body: StreamBuilder(
-        stream: rrId.orderBy('timestamp').snapshots(),
-        builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  var doc = snapshot.data!.docs;
-
-                  return Card(
-                    child: ListTile(
-                      title: Text(
-                        'Student ID: ' + doc[index]['userIndex'],
-                        style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              fontSize: 20,
-                              color: Color(0xff0b3140),
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      subtitle: Text(
-                        'Marked Time: ' + doc[index]['timestamp'],
-                        style: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              fontSize: 16,
-                              color: Colors.grey,
-                              fontWeight: FontWeight.normal),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: size.height / 17.5,
+                    width: size.width,
+                    child: Scrollbar(
+                      child: Center(
+                        child: Container(
+                          child: StreamBuilder(
+                            stream: rrId.snapshots(),
+                            builder:
+                                (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+                              if (snapshot.hasData) {
+                                return ListView.builder(
+                                    itemCount: snapshot.data!.docs.length,
+                                    itemBuilder: (context, index) {
+                                      return Center(
+                                        child: Text(
+                                          "Total: " +
+                                              '${snapshot.data!.docs.length}',
+                                          style: GoogleFonts.poppins(
+                                            textStyle: TextStyle(
+                                                fontSize: 21,
+                                                color: Color(0xff0b3140),
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      );
+                                    });
+                              } else {
+                                return Text("");
+                              }
+                            },
+                          ),
                         ),
                       ),
                     ),
-                  );
-                });
-          } else
-            return Text("");
-        },
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: size.height / 1.245,
+              width: size.width,
+              child: StreamBuilder(
+                stream: rrId.orderBy('timestamp').snapshots(),
+                builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          var doc = snapshot.data!.docs;
+
+                          return Card(
+                            child: ListTile(
+                              title: Text(
+                                'Student ID: ' + doc[index]['userIndex'],
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 20,
+                                      color: Color(0xff0b3140),
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Marked Time: ' + doc[index]['timestamp'],
+                                style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey,
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ),
+                            ),
+                          );
+                        });
+                  } else
+                    return Text("");
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
